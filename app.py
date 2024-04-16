5f1972802c361bba64f192acd6532c5e51ec5699
Nuha-hub <134822512+Nuha-hub@users.noreply.github.com>
2024-04-16 21:49:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,35 +7,35 @@ from requests import post
 from requests import get
 import random , hashlib , time
 
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/check_email/<email>', methods=['GET'])
 def chk(email):
-  ma = requests.Session()
-  g = str(''.join(random.choice('qwertyuiopasdfghjklzxcvbnm') for i in range(8)))
-  password = f"#PWD_INSTAGRAM_BROWSER:0:{int(time.time())}:{g}"
-  s3 = ma.get('https://www.instagram.com/accounts/login/')
-  rs3 = ma.get('https://www.instagram.com/accounts/login/')
-  ctk = rs3.text.replace("\\", "").split('csrf_token\":\"')[1].split('"')[0]
-  headers = {
-              "user-agent":generate_user_agent(),
-              "x-csrftoken": ctk,
-              "x-ig-www-claim": "0",
-          }
-  rs3 = ma.post(
-              "https://www.instagram.com/api/v1/web/accounts/login/ajax/",
-              headers=headers,
-  data={
-                  "enc_password": password,
-                  "username": email,
-                  "queryParams": "{}",
-                  "optIntoOneTap": "false",
-                  "trustedDeviceRecords": "{}"
-              },
-          )
+  ma = requests.Session()
+  g = str(''.join(random.choice('qwertyuiopasdfghjklzxcvbnm') for i in range(8)))
+  password = f"#PWD_INSTAGRAM_BROWSER:0:{int(time.time())}:{g}"
+  s3 = ma.get('https://www.instagram.com/accounts/login/')
+  rs3 = ma.get('https://www.instagram.com/accounts/login/')
+  ctk = rs3.text.replace("\\", "").split('csrf_token\":\"')[1].split('"')[0]
+  headers = {
+              "user-agent":generate_user_agent(),
+              "x-csrftoken": ctk,
+              "x-ig-www-claim": "0",
+          }
+  rs3 = ma.post(
+              "https://www.instagram.com/api/v1/web/accounts/login/ajax/",
+              headers=headers,
+  data={
+                  "enc_password": password,
+                  "username": email,
+                  "queryParams": "{}",
+                  "optIntoOneTap": "false",
+                  "trustedDeviceRecords": "{}"
+              },
+          )
 
-  headers.update({"x-ig-set-www-claim":"0"})
-  headers.update({"x-csrftoken": ctk})
-  return  jsonify(rs3.text)
-if __name__ == '__main__':
-    app.run(debug=True)
+  headers.update({"x-ig-set-www-claim":"0"})
+  headers.update({"x-csrftoken": ctk})
+  return  jsonify(rs3.text)
+#if name == 'main':
+#    app.run(debug=True)
