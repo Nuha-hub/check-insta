9e66843482f02cb80d6946a8d85f4f968220f029
Nuha-hub <134822512+Nuha-hub@users.noreply.github.com>
2024-04-16 21:37:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,37 @@ import uuid
 from user_agent import generate_user_agent
 from requests import post
 from requests import get
-
-
+import random , hashlib , time
 
 app = Flask(__name__)
 
 @app.route('/check_email/<email>', methods=['GET'])
 def chk(email):
-	mm=requests.post(url="https://www.instagram.com/api/v1/web/accounts/account_recovery_ajax/", headers={"X-Csrftoken": "d98HmasrM5VNPmA6jl6lwBmsyk8kkk0n"}, data={"query": f"{email}"}).text 
-	return jsonify(mm)	
+  ma = requests.Session()
+  g = str(''.join(random.choice('qwertyuiopasdfghjklzxcvbnm') for i in range(8)))
+  password = f"#PWD_INSTAGRAM_BROWSER:0:{int(time.time())}:{g}"
+  s3 = ma.get('https://www.instagram.com/accounts/login/')
+  rs3 = ma.get('https://www.instagram.com/accounts/login/')
+  ctk = rs3.text.replace("\\", "").split('csrf_token\":\"')[1].split('"')[0]
+  headers = {
+              "user-agent": user_agent.generate_user_agent(),
+              "x-csrftoken": ctk,
+              "x-ig-www-claim": "0",
+          }
+  rs3 = ma.post(
+              "https://www.instagram.com/api/v1/web/accounts/login/ajax/",
+              headers=headers,
+  data={
+                  "enc_password": password,
+                  "username": email,
+                  "queryParams": "{}",
+                  "optIntoOneTap": "false",
+                  "trustedDeviceRecords": "{}"
+              },
+          )
+
+  headers.update({"x-ig-set-www-claim":"0"})
+  headers.update({"x-csrftoken": ctk})
+  jsonify(re3.text)
 if __name__ == '__main__':
     app.run(debug=True)
