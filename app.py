8ab674cf65583ef354dbe80bff94fbdd2c6b277f
Nuha-hub <134822512+Nuha-hub@users.noreply.github.com>
2024-05-20 14:38:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def login():
     ctk = re.search(r'csrf_token":"(.*?)"', rs3.text).group(1)
 
     headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
         'accept-language': "ar",
         "user-agent": generate_user_agent(),
         "x-csrftoken": ctk,
